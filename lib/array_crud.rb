cc0e079f26cec919f89741b5cd60691248a62591
Andrew Capp <acapp909@gmail.com>
2019-09-20 22:36:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,23 +7,23 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  << "New Item"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  .unshift("New Item2")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  .pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  .shift   
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  []
 end
 
 def retrieve_first_element_from_array(array)
